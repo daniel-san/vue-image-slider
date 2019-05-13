70292f573343a3a384810e322e4db542ff3687e1
Daniel Lucas <dlsbdaniel@gmail.com>
2019-05-13 16:51:44 -0300

    First commit

---
+++ components/ImageSlider.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+  <p>
+    <a @click="prev">Previous</a> || <a @click="next">Next</a>
+  </p>
+  <transition-group name="fade" tag="div">
+  <div
+    v-for="number in [currentNumber]"
+    :key="number"
+    >
+    <img 
+      :src="currentImage" 
+      @mouseover="stopRotation" 
+      @mouseout="startRotation"/>
+  </div>
+  </transition-group>
+</div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+export default {
+
+  data() {
+    return { 
+      images: ['http://i.imgur.com/vYdoAKu.jpg', 'http://i.imgur.com/PUD9HQL.jpg', 'http://i.imgur.com/Lfv18Sb.jpg', 'http://i.imgur.com/tmVJtna.jpg', 'http://i.imgur.com/ZfFAkWZ.jpg'],
+      currentNumber: 0,
+      timer: null
+    }
+  },
+
+  computed: {
+    currentImage() {
+      return this.images[Math.abs(this.currentNumber) % this.images.length];
+    }
+  },
+
+  mounted() {
+    this.startRotation();
+  },
+
+  methods: {
+    startRotation() {
+      this.timer = setInterval(this.next,3000);
+    },
+    stopRotation() {
+      clearTimeout(this.timer);
+      this.timer = null;
+    },
+    next() {
+      this.currentNumber += 1;
+    },
+    prev() {
+      this.currentNumber -= 1;
+    },
+  }
+}
+</script>
